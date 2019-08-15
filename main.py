9edbeb3af20e19e47d59d62e5e6fa8754b2cf218
1303377506@qq.com <1303377506@qq.com>
2019-08-15 19:40:44 +0800

    add alphabet

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from src.get_words_list import get_all_words
 from src.alphabet import Alpha
+from src.get_data_frame import get_frame
 
 excel_path = '/home/tusimple/N2-Japanese-Classify/word/japanese_words.xlsx'
 
@@ -10,6 +11,13 @@ word_list = get_all_words(excel_path)
 
 # build the word table
 alpha = Alpha()
+alpha.classify(word_list)
+
+# get the dataframe
+df = get_frame(alpha)
+
+
+
 
 
 
